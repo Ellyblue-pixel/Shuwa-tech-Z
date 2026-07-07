9e12cbff1b48c267e9f0ce6e1b626b9e97d11d1e
Ellyblue-pixel <ellyblue256@gmail.com>
2026-07-07 20:35:21 +0300

    Implement menu toggle and card slideshow functionality
    Added functions to toggle menu and show next card.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+}
+*
+function toggleMenu() {
+  document.getElementById('menuBox').classList.toggle('active');
+}
+
+let currentCard = 0;
+const cards = document.querySelectorAll('.card');
+
+function showNextCard() {
+  cards[currentCard].classList.remove('active');
+  currentCard = (currentCard + 1) % cards.length;
+  cards[currentCard].classList.add('active');
+}
+
+setInterval(showNextCard, 5000); // 5 seconds
